57893b5838814cfb1ec6969b572d68965935323c
charishmasree <charishmasreedivi21@gmail.com>
2023-05-24 12:07:22 +0530

    cherry

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu_icon=document.querySelector('.navbar .menu_icon');
+let links=document.querySelector('.navbar .links');
+
+
+
+menu_icon.addEventListener('click',function(){
+ document.querySelector('.navbar').classList.toggle('show');
+});
